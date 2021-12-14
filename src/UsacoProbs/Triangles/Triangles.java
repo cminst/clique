cd0e0b93be2fba8a2bae03d62ee6947ef842c138
ruo2019 <ruo2019@outlook.com>
2021-12-13 21:13:30 -0800

    stuff

--- src/UsacoProbs/Triangles/Triangles.java
+++ src/UsacoProbs/Triangles/Triangles.java
@@ -7,13 +7,42 @@ import java.util.Scanner;
 
 public class Triangles {
 
-    static void addOccurences(int x, int y, HashMap<Integer, HashSet<Integer>> points) {
+    static void addOccurrences(int x, int y, HashMap<Integer, HashSet<Integer>> points) {
         if (points.containsKey(x)) {
             points.get(x).add(y);
         } else {
             HashSet<Integer> hashset = new HashSet<>();
             hashset.add(y);
             points.put(x, hashset);
+        }
+    }
+
+    static void check(int[] arr) {
+
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int first = arr[index1];
+        int second = arr[index2];
+        arr[index1] = second;
+        arr[index2] = first;
+    }
+
+    static void generate(int[] arr, int k) {
+        if (k == 1) {
+            check(arr);
+        }
+        else {
+            generate(arr, k-1);
+            for (int i = 0; i < k - 1; i++) {
+                if (k % 2 == 0) {
+                    swap(arr, i, k-1);
+                }
+                else {
+                    swap(arr, 0, k-1);
+                }
+                generate(arr, k-1);
+            }
         }
     }
 
@@ -25,70 +54,47 @@ public class Triangles {
         for (int i = 0; i < nPoints; i++) {
             int x = r.nextInt();
             int y = r.nextInt();
-            addOccurences(x, y, points);
+            addOccurrences(x, y, points);
         }
+        int previousY = 0;
+        int maxArea = 0;
         for (Integer x : points.keySet()) {
             HashSet<Integer> xs = new HashSet<>();
             HashSet<Integer> ys = points.get(x);
             for (Integer y : ys) {
-                for (Integer i : points.keySet()) {
-                    if (points.get(i).contains(y)) {
-                        xs.add(i);
+                if (previousY != y) {
+                    for (Integer i : points.keySet()) {
+                        if (points.get(i).contains(y)) {
+                            xs.add(i);
+                        }
                     }
+//                    pw.println(xs);
+                    int maxY = 0;
+                    int minY = Integer.MAX_VALUE;
+                    for (int i : ys) {
+                        if (i > maxY) {
+                            maxY = i;
+                        }
+                        if (i < minY) {
+                            minY = i;
+                        }
+                    }
+                    int maxX = 0;
+                    for (int i : xs) {
+                        if (i > maxX) {
+                            maxX = i;
+                        }
+                    }
+                    if (ys.size() > 1) {
+                        if ((maxY - minY) * (maxX - minY) > maxArea) {
+                            maxArea = (maxY - minY) * (maxX - minY);
+                        }
+                    }
+                    previousY = y;
                 }
-                pw.println(xs);
-                xs.clear();
             }
-//            pw.print(x);
-//            pw.println(ys);
         }
+        pw.println(maxArea);
         pw.close();
     }
 }
-//        int[] xs = new int[nPoints];
-//        int[] ys = new int[nPoints];
-//        int[][] arr2d = new int[20000][20000];
-//
-//        for (int i = 0; i < arr2d.length; i++) {
-//            for (int j = 0; j < arr2d.length; j++) {
-//                arr2d[i][j] = -1;
-//            }
-//        }
-//        for (int i = 0; i < nPoints; i++) {
-//            int x = r.nextInt();
-//            int y = r.nextInt();
-//            xs[i] = x;
-//            ys[i] = y;
-//            arr2d[x][y] = 1;
-//        }
-//        int maxXDist = 0;
-//        int maxYDist = 0;
-//        for (int i = 0; i < nPoints; i++) {
-//            for (int j = i + 1; j < nPoints; j++) {
-//                int dx = Math.abs(xs[i] - xs[j]);
-//                int dy = Math.abs(ys[i] - ys[j]);
-//                if (dy + dx > maxXDist+ maxYDist) {
-//                    maxXDist = dx;
-//                    maxYDist = dy;
-//                }
-//            }
-//        }
-//
-//        int maxXPoint = 0;
-//        int maxYPoint = 0;
-//        for (int i = 0; i < nPoints; i++) {
-//            int x = xs[i];
-//            int y = ys[i];
-//            if (x+y > maxXPoint+maxYPoint) {
-//                maxXPoint = x;
-//                maxYPoint = y;
-//            }
-//        }
-//
-//        for (int i = 0; i < xs.length; i++) {
-//            if (xs[i] == maxXPoint-maxXDist && ys[i] == maxYPoint-maxYDist) {
-//                if ((xs[i] == maxXPoint && ys[i] == maxYPoint-maxYDist) || (xs[i] == maxXPoint-maxXDist && ys[i] == maxYPoint)) {
-//                    pw.println(maxXDist*maxYDist);
-//                }
-//            }
-//        }
