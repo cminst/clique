d8d4303b760b99fc7451e6e867bc18b9ba86605b
ruo2019 <ruo2019@outlook.com>
2021-12-28 10:20:05 -0800

    solved Lifeguards.java again

--- src/UsacoProbs/Lifeguards/Lifeguards.java
+++ src/UsacoProbs/Lifeguards/Lifeguards.java
@@ -10,36 +10,37 @@ public class Lifeguards {
         Scanner r = new Scanner(new FileReader("lifeguards.in"));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("lifeguards.out")));
 
-        int nLifeguards = r.nextInt();
-        HashMap<Integer, Integer> pairs = new HashMap<>();
+        int n = r.nextInt();
+        HashMap<Integer, Integer> lifegaurds = new HashMap<>();
 
-        for (int i = 0; i < nLifeguards; i++) {
-            int start = r.nextInt();
-            int end = r.nextInt();
-            pairs.put(start, end);
+        for (int i = 0; i < n; i++) {
+            int starttime = r.nextInt();
+            int endtime = r.nextInt();
+            lifegaurds.put(starttime, endtime);
         }
 
-        int maxLifeguardTime = 0;
-        for (int i : pairs.keySet()) {
-            int[] shifts = new int[1000];
-            int lifeguardTime = 0;
+        int max = 0;
+        for (int i: lifegaurds.keySet()) {
+            int[] time = new int[1000];
+            int numLifeguard = 0;
 
-            for (int j : pairs.keySet()) {
+            for (int j : lifegaurds.keySet()) {
                 if (j != i) {
-                    for (int k = j; k < pairs.get(j); k++) {
-                        if (shifts[k] == 0){
-                            lifeguardTime++;
+                    for (int k = j; k < lifegaurds.get(j); k++) {
+                        if (time[k] == 0) {
+                            numLifeguard++;
                         }
-                        shifts[k] += 1;
+                        time[k] = 1;
                     }
                 }
             }
 
-            if (lifeguardTime > maxLifeguardTime) {
-                maxLifeguardTime = lifeguardTime;
+            if (numLifeguard > max) {
+                max = numLifeguard;
             }
         }
-        pw.println(maxLifeguardTime);
+
+        pw.println(max);
         pw.close();
     }
 }
