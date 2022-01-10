4b22f1f3aa68e0af6c2e060e9645c461780467b6
ruo2019 <ruo2019@outlook.com>
2022-01-09 21:38:19 -0800

    edited RecordMilking.java

--- src/UsacoProbs/BronzeExam/RecordMilking.java
+++ src/UsacoProbs/BronzeExam/RecordMilking.java
@@ -41,33 +41,3 @@ public class RecordMilking {
         System.out.println(maxDiff);
     }
 }
-
-// import java.util.Scanner;
-//
-//public class RecordMilking {
-//
-//    public static void main(String[] args) {
-//        Scanner r = new Scanner(System.in);
-//        int recordMilking = r.nextInt();
-//        int bessieMilking = r.nextInt();
-//        int[] recordRates = new int[recordMilking];
-//        int[] bessieRates = new int[bessieMilking];
-//        for (int i = 0; i < recordMilking; i++) {
-//            r.nextInt();
-//            int recordRate = r.nextInt();
-//            recordRates[i] = recordRate;
-//        }
-//
-//        for (int i = 0; i < bessieMilking; i++) {
-//            r.nextInt();
-//            int bessieRate = r.nextInt();
-//            bessieRates[i] = bessieRate;
-//        }
-//
-//        int rateChange = 0;
-//        for (int i = 0; i < Math.min(recordRates.length, bessieRates.length); i++) {
-//            rateChange = Math.max(bessieRates[i]-recordRates[i], rateChange);
-//        }
-//        System.out.println(rateChange);
-//    }
-//}
