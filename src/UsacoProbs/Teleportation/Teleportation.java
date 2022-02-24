fccb61539ddae127dadcd16bd021cb686d8c2476
ruo2019 <ruo2019@outlook.com>
2022-02-24 14:18:15 -0800

    solved Teleportation.java

---
+++ src/UsacoProbs/Teleportation/Teleportation.java
@@ -0,0 +1,21 @@
+package UsacoProbs.Teleportation;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Teleportation {
+
+    public static void main(String[] args) throws IOException {
+        Scanner r = new Scanner(new FileReader("teleport.in"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("teleport.out")));
+
+        int start = r.nextInt();
+        int barn = r.nextInt();
+        int teleportX = r.nextInt();
+        int teleportY = r.nextInt();
+        var min = Math.min(barn-start, Math.abs(start-teleportX) + Math.abs(barn-teleportY));
+        min = Math.min(Math.abs(min), Math.abs(start-teleportY) + Math.abs(barn-teleportX));
+        pw.println(Math.abs(min));
+        pw.close();
+    }
+}
