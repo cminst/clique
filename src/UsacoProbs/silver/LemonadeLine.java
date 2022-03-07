6a7cda85687e9021907358fd510229d533062c85
ruo2019 <ruo2019@outlook.com>
2022-03-06 17:08:16 -0800

    solved LemonadeLine.java

---
+++ src/UsacoProbs/silver/LemonadeLine.java
@@ -0,0 +1,31 @@
+package UsacoProbs.silver;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class LemonadeLine {
+
+    public static void main(String[] args) throws IOException {
+        Scanner r = new Scanner(new FileReader("lemonade.in"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("lemonade.out")));
+
+        var nCows = r.nextInt();
+        var cows = new ArrayList<Integer>();
+        for (int i = 0; i < nCows; i++) {
+            cows.add(r.nextInt());
+        }
+        Collections.sort(cows);
+        Collections.reverse(cows);
+
+        var count = 0;
+        for (Integer cow : cows) {
+            if (count <= cow) {
+                count++;
+            }
+        }
+        pw.println(count);
+        pw.close();
+    }
+}
