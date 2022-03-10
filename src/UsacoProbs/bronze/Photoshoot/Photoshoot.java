4fce26d380c51a1327bdf155c775043d1b57aa65
ruo2019 <ruo2019@outlook.com>
2022-03-09 20:23:21 -0800

    Photoshoot

--- src/UsacoProbs/bronze/Photoshoot/Photoshoot.java
+++ src/UsacoProbs/bronze/Photoshoot/Photoshoot.java
@@ -1,7 +1,8 @@
-package UsacoProbs.Photoshoot;
+package UsacoProbs.bronze.Photoshoot;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Photoshoot {
 
@@ -10,7 +11,7 @@ public class Photoshoot {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("photo.out")));
         int nCows = r.nextInt();
         int[] a = new int[nCows];
-        int[] b = new int[nCows-1];
+        int[] b = new int[nCows - 1];
 
         for (int i = 0; i < nCows - 1; i++) {
             b[i] = r.nextInt();
@@ -18,16 +19,15 @@ public class Photoshoot {
         for (int i = 1; i < b[0]; i++) {
 
             int count = i;
-            a[0] = b[0]-i;
+            a[0] = b[0] - i;
             a[1] = i;
             for (int j = 2; j < nCows; j++) {
-                int num = b[j-1];
-                int key = num-count;
-                if (num-count > 0 && Arrays.stream(a).noneMatch(k -> k == key)) {
+                int num = b[j - 1];
+                int key = num - count;
+                if (num - count > 0 && Arrays.stream(a).noneMatch(k -> k == key)) {
                     a[j] = num - count;
                     count = a[j];
-                }
-                else {
+                } else {
                     a = new int[nCows];
                     break;
                 }
@@ -41,7 +41,7 @@ public class Photoshoot {
         for (int j : a) {
             s.append(j).append(" ");
         }
-        s.deleteCharAt(s.length()-1);
+        s.deleteCharAt(s.length() - 1);
         pw.println(s);
         pw.close();
     }
