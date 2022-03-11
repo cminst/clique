c51ad8f0b085ef1775f257372d402012517c6b5a
ruo2019 <ruo2019@outlook.com>
2022-03-10 17:44:14 -0800

    Teleportation

--- src/UsacoProbs/bronze/Teleportation/Teleportation.java
+++ src/UsacoProbs/bronze/Teleportation/Teleportation.java
@@ -1,4 +1,4 @@
-package UsacoProbs.Teleportation;
+package UsacoProbs.bronze.Teleportation;
 
 import java.io.*;
 import java.util.Scanner;
@@ -13,8 +13,8 @@ public class Teleportation {
         int barn = r.nextInt();
         int teleportX = r.nextInt();
         int teleportY = r.nextInt();
-        var min = Math.min(barn-start, Math.abs(start-teleportX) + Math.abs(barn-teleportY));
-        min = Math.min(Math.abs(min), Math.abs(start-teleportY) + Math.abs(barn-teleportX));
+        var min = Math.min(barn - start, Math.abs(start - teleportX) + Math.abs(barn - teleportY));
+        min = Math.min(Math.abs(min), Math.abs(start - teleportY) + Math.abs(barn - teleportX));
         pw.println(Math.abs(min));
         pw.close();
     }
