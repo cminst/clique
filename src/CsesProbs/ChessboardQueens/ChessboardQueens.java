72d429a0edfcffc405ce1c13832c812c0072437d
ruo2019 <ruo2019@outlook.com>
2022-11-12 11:53:04 -0800

    ChessboardQueens

--- src/CsesProbs/ChessboardQueens/ChessboardQueens.java
+++ src/CsesProbs/ChessboardQueens/ChessboardQueens.java
@@ -10,39 +10,6 @@ import java.util.StringTokenizer;
 
 public class ChessboardQueens {
 
-    static class InputReader {
-        BufferedReader reader;
-        StringTokenizer tokenizer;
-
-        public InputReader(InputStream stream) {
-            reader = new BufferedReader(new InputStreamReader(stream), 32768);
-            tokenizer = null;
-        }
-
-        String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return tokenizer.nextToken();
-        }
-
-        public int nextInt() { // reads in the next int
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() { // reads in the next long
-            return Long.parseLong(next());
-        }
-
-        public double nextDouble() { // reads in the next double
-            return Double.parseDouble(next());
-        }
-    }
-
     static InputReader r = new InputReader(System.in);
     static int count = 0;
     static ArrayList<Point> reserved = new ArrayList<>();
@@ -84,6 +51,39 @@ public class ChessboardQueens {
             }
         } else {
             count++;
+        }
+    }
+
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() { // reads in the next int
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() { // reads in the next long
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() { // reads in the next double
+            return Double.parseDouble(next());
         }
     }
 
