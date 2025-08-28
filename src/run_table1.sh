b8c844d8eb216b5080623d48900fae518547e7ca
Chungust <qingy2019@outlook.com>
2025-08-27 22:03:27 -0700

    Removed unused file and added new CSV (blank for now), 1e-1 csv for new table1 results

--- src/run_table1.sh
+++ src/run_table1.sh
@@ -1,1 +1,1 @@
-javac UsacoProbs/Table1Synthetic.java UsacoProbs/clique2_mk_benchmark_accuracy.java && java UsacoProbs.Table1Synthetic
+javac UsacoProbs/Table1Synthetic.java UsacoProbs/clique2_mk_benchmark_accuracy.java && java UsacoProbs.Table1Synthetic 1e-1
